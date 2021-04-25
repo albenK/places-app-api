32318ee0ad50e5b3e7934e06bc378ed5a23f1a65
Alben Kalambukadu <albenk234@gmail.com>
2021-04-25 18:30:48 -0400

    Added error handling

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const DUMMY_PLACES = require('../dummy-places');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 router.get('/:placeId', (req, res, next) => {
     const placeId = req.params.placeId;
     const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+    if (!place) {
+        const error = new HttpError('Could not find a place for the provided place id.', 404);
+        throw error;
+    }
+
     res.json({place});
 });
 
 router.get('/user/:userId', (req, res, next) => {
     const userId = req.params.userId;
     const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+    if (!place) {
+        const error = new HttpError('Could not find a place for the provided user id.', 404);
+        return next(error);
+    }
+
     res.json({place});
 });
 
